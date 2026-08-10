e708ad1b69062075097a850c8b9b6b34b37af483
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-08-10 21:26:37 +0100

    Ketsu

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/LancerClassPursuitCraft/KetsuOnyo.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/LancerClassPursuitCraft/KetsuOnyo.cs
@@ -47,7 +47,7 @@ namespace Ship.SecondEdition.LancerClassPursuitCraft
     {
         public KetsuOnyoXWA() : base()
         {
-            (PilotInfo as PilotCardInfo25).Cost = 15;
+            (PilotInfo as PilotCardInfo25).Cost = 16;
             (PilotInfo as PilotCardInfo25).LoadoutValue = 12;
             (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
         }
