5421993d71c5506d2cee94bd37b17fbfd3aee110
Jeremy S. <baledin@gmail.com>
2026-08-16 08:36:15 -0700

    Corrected XWA Version string

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -18,7 +18,7 @@ public class Global : MonoBehaviour
     public static int LatestVersionInt = 0;
 
     // Used for json exports, update when vendor version changes
-    public static string CurrentXWAVersion = "50P-2.2";
+    public static string CurrentXWAVersion = "50P-2.1";
     public static string CurrentAMGVersion = "2.8.0";
 
     public static SquadBuilder SquadBuilder { get; set; }
